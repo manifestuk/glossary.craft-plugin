b7e4fee8c0131b786d5d5bf5a5673a86f0a76390
Stephen Lewis <stephen@experiencehq.net>
2014-09-04 09:25:40 +0100

    Move donkey work to service.

--- glossary/variables/GlossaryVariable.php
+++ glossary/variables/GlossaryVariable.php
@@ -2,31 +2,16 @@
 
 class GlossaryVariable
 {
-    public function make($items, $groupField)
+    /**
+     * Builds a glossary of the given items, grouped by the specified field.
+     *
+     * @param array|ElementCriteriaModel $items
+     * @param string $groupField
+     *
+     * @return array
+     */
+    public function build($items, $groupField)
     {
-        return $this->buildGlossary($items, $groupField);
-    }
-
-    protected function buildGlossary($items, $groupField)
-    {
-        $glossary = [];
-        $groupedItems = array_fill_keys(range('A', 'Z'), []);
-
-        foreach ($items as $item) {
-            $fieldData = $item->$groupField;
-
-            $fieldIndex = strtoupper(substr($fieldData, 0, 1));
-
-            $groupedItems[$fieldIndex][] = $item;
-        }
-
-        foreach ($groupedItems as $index => $items) {
-            $glossary[] = new Glossary_LetterModel([
-                'label' => $index,
-                'items' => $items,
-            ]);
-        }
-
-        return $glossary;
+        return craft()->glossary_glossary->buildGlossary($items, $groupField);
     }
 }
